b811c9fde0031fa07d65d472ce2c49f8bd88a807
unknown <aguilarandres296@gmail.com>
2016-03-03 10:56:26 -0600

    Fin del programa

--- Practica/funciones.js
+++ Practica/funciones.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
    $('#iniciar').click(function() {
-     $( '#circulo' ).draggable({ containment: "#circulo".parent });
     pelota();
 });
 
@@ -10,16 +9,77 @@ $(document).ready(function() {
         });
 
    function pelota() {
-    $('#circulo').animate({left:"92%"},"slow",function(){
-        $('#circulo').animate({top: "87%"},"slow",function(){
-            $('#circulo').animate({left: '1px'},"slow",function(){
-                $('#circulo').animate({top: '1px'},"slow",function(){
+    $('#circulo').animate({left:"87%"},"slow",function(){
+        $('#circulo').animate({top: "83%"},"slow",function(){
+            $('#circulo').animate({left: '5px'},"slow",function(){
+                $('#circulo').animate({top: '5px'},"slow",function(){
                     pelota();
                 });
             });
         });
     });
 }
+
+
+function guarda_datos(){
+    var nombre = document.getElementById("name").value;
+    var date = document.getElementById("date").value;
+
+    document.getElementById("name").value = "";
+    document.getElementById("date").value = "";
+
+    document.getElementById('nombre').innerHTML="Nombre: "+nombre;
+    document.getElementById('fecha').innerHTML="Fecha: "+date;
+
+}
+
+$("#datos").resizable({containment:"parent"});
+$("#aceptar").click(()=>{
+    $("#nombre").empty();
+    $("fecha").empty();
+    var name = $("#name").val();
+    var date =  $("#date").val();
+    $("#nombre").append("Nombre: " + name);
+    $("#fecha").append("Fecha: " + date);
+
+
+    console.log($("#name").attr("value"));
+
+})
+
+$("#name").tooltip({
+    altField: "#actualDate"
+});
+
+$("#date").tooltip({
+    altField: "#actualDate"
+});
+
+var date =  $("#date").val();
+
+
+$("#mostrar").click(function(){
+        $("#figura_1").fadeIn(3000);
+        $("#figura_2").fadeIn(2000);
+        $("#figura_3").fadeIn(1000);
+      });
+
+    $("#ocultar").click(function(){
+        $("#figura_1").fadeOut(1000);
+        $("#figura_2").fadeOut(2000);
+        $("#figura_3").fadeOut(3000);
+      }); 
+
+$("#creditos").hover(function(){
+    $("#creditos").animate({left: '80%'},"slow");
+}, ()=>{
+    $("#creditos").animate({left: '95%'},"slow");
+});
+
+
+
+$("#date").datepicker();
+$( "#datos" ).draggable();
 
 
 
