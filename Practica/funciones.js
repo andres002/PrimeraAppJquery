865f181f86e2866737001901a10db968a79f9be0
unknown <aguilarandres296@gmail.com>
2016-03-03 00:02:37 -0600

    boton parar

--- Practica/funciones.js
+++ Practica/funciones.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
 
-     $('#iniciar').click(function() {
-        pelota();
-     });
+   $('#iniciar').click(function() {
+     $( '#circulo' ).draggable({ containment: "#circulo".parent });
+    pelota();
+});
 
-    function pelota() {
-        $('#circulo').animate({'marginLeft' : "+=92%"},"slow",function(){
-            $('#circulo').animate({'marginTop' : "+=57%"},"slow",function(){
-                $('#circulo').animate({'marginLeft' : "-=92%"},"slow",function(){
-                    $('#circulo').animate({'marginTop' : "-=57%"},"slow",function(){
-                        pelota();
-                   });
-               });
-           });
+      $('#parar').click(function() {
+        $('#circulo').stop();
         });
-    }
+
+   function pelota() {
+    $('#circulo').animate({left:"92%"},"slow",function(){
+        $('#circulo').animate({top: "87%"},"slow",function(){
+            $('#circulo').animate({left: '1px'},"slow",function(){
+                $('#circulo').animate({top: '1px'},"slow",function(){
+                    pelota();
+                });
+            });
+        });
+    });
+}
+
+
+
 });
